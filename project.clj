e5841358468e6bb438cbf98c7268f6d569a2a176
Brian Marick <marick@exampler.com>
2015-11-28 18:27:23 -0600

    bump some dependency versions

--- project.clj
+++ project.clj
@@ -8,17 +8,17 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.rpl/specter "0.8.0" :exclusions [org.clojure/clojure org.clojure/clojurescript]]
-                 [marick/suchwow "4.4.0" :exclusions [org.clojure/clojure org.clojure/clojurescript]]
-                 [marick/clojure-commons "1.1.1" :exclusions [org.clojure/clojure]]
+                 [marick/suchwow "4.4.1" :exclusions [org.clojure/clojure org.clojure/clojurescript]]
+                 [marick/clojure-commons "1.1.2" :exclusions [org.clojure/clojure]]
 ]
-  :profiles {:dev {:dependencies [[midje "1.8.1" :exclusions [org.clojure/clojure]]]}
+  :profiles {:dev {:dependencies [[midje "1.8.2" :exclusions [org.clojure/clojure]]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta1"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta2"]]}
              }
   :plugins [[lein-midje "3.2"]]
 
-  :aliases {"compatibility" ["with-profile" "+1.6+1.7:+1.8" "midje" ":config" ".compatibility-test-config"]
+  :aliases {"compatibility" ["with-profile" "+1.6:+1.7:+1.8" "midje" ":config" ".compatibility-test-config"]
             "travis" ["with-profile" "+1.6:+1.7:+1.8" "midje"]}
 
   ;; For Clojure snapshots
